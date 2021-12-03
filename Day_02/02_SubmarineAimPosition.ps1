d69361c09ea2aab99b86c5a49e0bc0c2db6a5a74
Lennart Joneleit <lennart.joneleit@atos.net>
2021-12-03 23:43:49 +0100

    Completed Day02

---
+++ Day_02/02_SubmarineAimPosition.ps1
@@ -0,0 +1,54 @@
+<#
+    .SYNOPSIS
+    AdventOfCode 2021 Day 02 part 2
+
+    .DESCRIPTION
+    We're trying to figure out where the submarine is based on the steering commands provided, now a third parameter "aim" is added.
+    The result is the calculated horizontal position multiplied with the depth.
+
+    .PARAMETER InputPath
+    Specifies the path to the plain text input file.
+
+    .INPUTS
+    None. You cannot pipe objects to submarinePosition.ps1
+
+    .OUTPUTS
+    The solution is returned as [int]
+
+    .EXAMPLE
+    PS> .\submarineAimPosition.ps1
+#>
+
+[CmdletBinding()]
+param (
+    [Parameter()] [System.IO.FileInfo] $payloadFilePath = "./01_SubmarinePayload.txt"
+)
+
+Begin {
+    [string[]] $steerings = Get-Content $payloadFilePath
+    [int] $horizontal = 0
+    [int] $depth = 0
+    [int] $aim = 0
+}
+
+Process {
+    $steerings | ForEach-Object {
+        [array] $steeringInstruction = $_.Split(" ")
+        switch ($steeringInstruction[0]) {
+            forward {
+                $horizontal += $steeringInstruction[1]
+                $depth += [int]::Parse($steeringInstruction[1]) * $aim
+            }
+            down {
+                $aim += $steeringInstruction[1]
+            }
+            up {
+                $aim -= $steeringInstruction[1]
+            }
+        }
+    }
+}
+
+End {
+    Write-Output -InputObject ($horizontal * $depth)
+}
